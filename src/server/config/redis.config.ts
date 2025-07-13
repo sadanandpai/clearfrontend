82881770b287ed154339aa5a69c5fbaef29d7c72
Sadanand Pai <sadypai@gmail.com>
2025-07-13 15:21:46 +0530

    Redis host end point update

--- src/server/config/redis.config.ts
+++ src/server/config/redis.config.ts
@@ -1,3 +1,3 @@
 export const REDIS_HOST =
-  "redis-14312.c305.ap-south-1-1.ec2.redns.redis-cloud.com";
-export const REDIS_PORT = 14312;
+  "redis-17267.crce182.ap-south-1-1.ec2.redns.redis-cloud.com";
+export const REDIS_PORT = 17267;
