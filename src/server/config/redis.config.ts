fb7e7ca24bfef4ab6641128a151b3a559ee8855c
Gopalakrishnan <krishnan.gopal34@gmail.com>
2025-09-14 14:30:17 +0530

    Redis Host and Port changed

--- src/server/config/redis.config.ts
+++ src/server/config/redis.config.ts
@@ -1,3 +1,3 @@
 export const REDIS_HOST =
-  "redis-17267.crce182.ap-south-1-1.ec2.redns.redis-cloud.com";
-export const REDIS_PORT = 17267;
+  "redis-17830.crce179.ap-south-1-1.ec2.redns.redis-cloud.com";
+export const REDIS_PORT = 17830;
