b9fc6ca064e6fed99e6d3405927d71c1c34772c4
jrajni <jrajni6@gmail.com>
2025-10-07 14:55:44 +0530

    removed signup & login based on authentication

--- src/ui/components/common/home-nav-bar/home-nav-bar.tsx
+++ src/ui/components/common/home-nav-bar/home-nav-bar.tsx
@@ -1,11 +1,16 @@
+"use client";
 import { Flex } from "@radix-ui/themes";
 import Image from "next/image";
 import Link from "next/link";
 import { MenuDropdown } from "./menu-dropdown";
 import classes from "./home-nav-bar.module.scss";
 import { routes } from "@/common/routes";
+import { useContext } from "react";
+import { appContext } from "@/ui/context/app.context";
 
 export function HomeNavBar() {
+  const { user, isLoginChecked } = useContext(appContext);
+const isAuthenticated = !!user;
   return (
     <nav className={classes.navbar} role="navigation">
       <Flex align="center" gap="5">
@@ -27,6 +32,8 @@ export function HomeNavBar() {
         </Link>
       </Flex>
       <Flex align="center" gap="5" className="hidden lg:flex">
+      {isLoginChecked && !isAuthenticated && (
+        <> 
         <Link href={routes.signIn} className="secondary-link">
           LOGIN
         </Link>
@@ -34,6 +41,8 @@ export function HomeNavBar() {
         <Link href={routes.signUp} className="primary-link">
           SIGN UP
         </Link>
+        </>
+      )}
 
         <Link
           href="https://github.com/sadanandpai/clearfrontend"
