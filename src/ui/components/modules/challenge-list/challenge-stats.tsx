3eb52defd518b241f4d518cca41ea17a87bd4876
Karan Chadha <karan10chadha@gmail.com>
2025-11-02 05:00:34 +0530

    feat: progress tracker on challenge page

--- src/ui/components/modules/challenge-list/challenge-stats.tsx
+++ src/ui/components/modules/challenge-list/challenge-stats.tsx
@@ -5,7 +5,7 @@ import { Challenges } from "./challenge-list.types";
 
 interface Props {
   challenges: Challenges[];
-  solvedChallengeIds?: number[]
+  solvedChallengeIds?: number[];
 }
 
 export const ChallengeStats = ({ challenges, solvedChallengeIds = [] }: Props) => {
@@ -33,63 +33,57 @@ export const ChallengeStats = ({ challenges, solvedChallengeIds = [] }: Props) =
   };
 
   return (
-    <Flex gap="3" style={{ margin: "24px 0", padding: "0 16px" }}>
-      {/* Easy */}
-      <Card style={{ padding: 16, flex: 1 }}>
-        <Flex direction="column" gap="2">
-          <Flex justify="between" align="center">
-            <Text size="2" weight="medium" color="green">
-              Easy
-            </Text>
-            <Text size="2" weight="bold">
-              {easyStats.solved}/{easyStats.total}
-            </Text>
-          </Flex>
-          <Progress
-            value={easyStats.percentage}
-            color={getDifficultyColor("Easy")}
-            size="1"
-          />
-        </Flex>
-      </Card>
+  <Flex gap="3" style={{ flex: 1 }}>
+    {/* Easy */}
+    <Card style={{ padding: "16px", flex: 1 }}>
+      <Flex direction="column" gap="2">
+        <Text size="2" weight="medium" color="green">
+          Easy
+        </Text>
+        <Text size="5" weight="bold">
+          {easyStats.solved}/{easyStats.total}
+        </Text>
+        <Progress
+          value={easyStats.percentage}
+          color={getDifficultyColor("Easy")}
+          size="2"
+        />
+      </Flex>
+    </Card>
 
-      {/* Medium */}
-      <Card style={{ padding: 16, flex: 1 }}>
-        <Flex direction="column" gap="2">
-          <Flex justify="between" align="center">
-            <Text size="2" weight="medium" color="yellow">
-              Medium
-            </Text>
-            <Text size="2" weight="bold">
-              {mediumStats.solved}/{mediumStats.total}
-            </Text>
-          </Flex>
-          <Progress
-            value={mediumStats.percentage}
-            color={getDifficultyColor("Medium")}
-            size="1"
-          />
-        </Flex>
-      </Card>
+    {/* Medium */}
+    <Card style={{ padding: "16px", flex: 1 }}>
+      <Flex direction="column" gap="2">
+        <Text size="2" weight="medium" color="yellow">
+          Medium
+        </Text>
+        <Text size="5" weight="bold">
+          {mediumStats.solved}/{mediumStats.total}
+        </Text>
+        <Progress
+          value={mediumStats.percentage}
+          color={getDifficultyColor("Medium")}
+          size="2"
+        />
+      </Flex>
+    </Card>
 
-      {/* Hard */}
-      <Card style={{ padding: 16, flex: 1 }}>
-        <Flex direction="column" gap="2">
-          <Flex justify="between" align="center">
-            <Text size="2" weight="medium" color="red">
-              Hard
-            </Text>
-            <Text size="2" weight="bold">
-              {hardStats.solved}/{hardStats.total}
-            </Text>
-          </Flex>
-          <Progress
-            value={hardStats.percentage}
-            color={getDifficultyColor("Hard")}
-            size="1"
-          />
-        </Flex>
-      </Card>
-    </Flex>
+    {/* Hard */}
+    <Card style={{ padding: "16px", flex: 1 }}>
+      <Flex direction="column" gap="2">
+        <Text size="2" weight="medium" color="red">
+          Hard
+        </Text>
+        <Text size="5" weight="bold">
+          {hardStats.solved}/{hardStats.total}
+        </Text>
+        <Progress
+          value={hardStats.percentage}
+          color={getDifficultyColor("Hard")}
+          size="2"
+        />
+      </Flex>
+    </Card>
+  </Flex>
   );
 };
